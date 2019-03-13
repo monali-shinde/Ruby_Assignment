4be6de1d2d6bceef678178659bb83e340653a445
mona <ms@gmail.com>
2019-03-13 19:51:38 +0530

    making changes to print all employee details

--- assign.rb
+++ assign.rb
@@ -9,4 +9,14 @@ headers=csv_data.shift.map{|csv_data| csv_data.to_s}
 
 string_data=csv_data.map { |row| row.map{|cell| cell.to_s}  }
 
-print array_of_hash=string_data.map{|row| Hash[*headers.zip(row).flatten]}
+array_of_hash=string_data.map{|row| Hash[*headers.zip(row).flatten]}
+
+#2 Print details of each employee
+
+#array_of_hash.each|key,value| puts "#{key} : #{value}"
+
+array_of_hash.each do |info|
+  info.each do |k,v|
+    puts "#{k} : #{v}"
+  end
+end
